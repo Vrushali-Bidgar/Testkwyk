95b0cae77083220740ddbd67c394d5892f12da18
♡ C O C O ♡ <81639162+Vrushali-Bidgar@users.noreply.github.com>
2023-01-29 18:53:10 +0530

    Website Mkaing From trash

---
+++ merchant_signin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="merchant_signin.css">
+</head>
+<body>
+    <button>click me</button>
+    <img class="bgimg" src="bg_img.jpg" alt="">
+
+</body>
+</html>
